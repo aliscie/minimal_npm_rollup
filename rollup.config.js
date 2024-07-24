c944ea651ca2d55b0922d384a27830a9b51e0576
aliscie <alihushamsci@icloud.com>
2024-07-24 18:51:41 +0800

    first commit

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from "rollup-plugin-terser";
+import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/index.tsx', // your main TypeScript file
@@ -11,7 +12,10 @@ export default {
     format: 'cjs'
   },
   plugins: [
-    typescript({ 
+    postcss({
+      extensions: ['.css'],
+    }),
+    typescript({
       tsconfig: "tsconfig.json" // your tsconfig file
     }),
     commonjs(),
